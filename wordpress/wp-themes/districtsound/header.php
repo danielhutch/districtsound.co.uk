80028f94cfaea1aa969c4b76284ec89063a91e5a
danielhutch <hutch.daniel@gmail.com>
2014-02-05 17:01:34 +0000

    content="width=device-width, initial-scale=1.0"/>

--- wordpress/wp-themes/districtsound/header.php
+++ wordpress/wp-themes/districtsound/header.php
@@ -8,7 +8,9 @@
 				bloginfo( 'name' );
 			?>
 
-	</title>	
+	</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<!-- Adding "maximum-scale=1" fixes the Mobile Safari auto-zoom bug: http://filamentgroup.com/examples/iosScaleBug/ -->
 		<?php wp_head(); ?>
 </head>
 <body>
