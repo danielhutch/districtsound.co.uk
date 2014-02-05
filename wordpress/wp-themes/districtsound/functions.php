d20f5d58e7d73ca06424eb6f7f62fd0c37fd9868
danielhutch <hutch.daniel@gmail.com>
2014-02-05 16:51:23 +0000

    Loading in JS

--- wordpress/wp-themes/districtsound/functions.php
+++ wordpress/wp-themes/districtsound/functions.php
@@ -1,5 +1,5 @@
 <?php
-
+// Load the Theme CSS
 function theme_styles() {
 
 	wp_enqueue_style( 'normalize', get_template_directory_uri() . '/css/normalize.css' );
@@ -11,6 +11,21 @@ function theme_styles() {
 	}
 
 }
+
+// Load the Theme JS
+function theme_js() {
+
+	wp_register_script( 'flexslider', get_template_directory_uri() . '/js/flexslider.js', array('jquery'), '', true  );
+	if( is_page('home')) {
+		wp_enqueue_script ('flexslider');
+	}
+	wp_enqueue_script ('theme_js', get_template_directory_uri() . '/js/theme.js', array('jquery'), '', true  );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_js' );
+
+
+
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
 
 // Enable custom menus
