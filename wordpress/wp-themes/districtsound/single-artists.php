b3724bdf9eb6ee2670c09061673cd9f0623c794b
danielhutch <hutch.daniel@gmail.com>
2014-03-30 12:41:50 +0100

    Single Artists page style

--- wordpress/wp-themes/districtsound/single-artists.php
+++ wordpress/wp-themes/districtsound/single-artists.php
@@ -1,20 +1,22 @@
 <?php get_header(); ?>
 
-<p>This is the single-releases.php file</p>
+<!-- <p>This is the single-artists.php file</p> -->
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<h3><?php the_title(); ?></h3>
-	<img src="<?php the_field( 'artist_shot_large' ); ?>"></a>
-	<?php the_field( 'artist_name' ); ?>
-	<?php the_field( 'artist_info' ); ?>
-	<hr>
+<div id="article">
 
-<?php endwhile; else: ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<p>There are no posts or pages here</p>
+		<!-- <h3><?php the_title(); ?></h3> -->
+		<img src="<?php the_field( 'artist_shot_large' ); ?>"></a>
+		<h2><?php the_field( 'artist_name' ); ?></h2>
+		<p><?php the_field( 'artist_info' ); ?><p>
 
-<?php endif; ?>
+	<?php endwhile; else: ?>
 
+		<p>There are no posts or pages here</p>
+
+	<?php endif; ?>
+</div>
 
 <?php get_footer(); ?>
