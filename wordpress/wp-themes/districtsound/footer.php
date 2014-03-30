17c7227cf8eddd180fa6c4667b4a44349c20483b
danielhutch <hutch.daniel@gmail.com>
2014-03-30 12:53:06 +0100

    Footer styles

--- wordpress/wp-themes/districtsound/footer.php
+++ wordpress/wp-themes/districtsound/footer.php
@@ -2,9 +2,16 @@
 	<?php wp_footer() ;?>
 	<div id="footer">
 		<hr>
-		<p>&copy;2014 Discrict Sound <br>
-		<a href="mailto:info@districtsound.co.uk">info@districtsound.co.uk</a><br>
-		Site by <a href="http://www.danielhutchinson.co.uk">Daniel</a></p>
+		<div class="footerlinks">
+			<p>&copy;2014 Discrict Sound <br>
+			<a href="mailto:info@districtsound.co.uk">info@districtsound.co.uk</a><br>
+			<a href="http://www.danielhutchinson.co.uk">Site by Daniel</a></p>
+		</div>
+		<div class="footerlinks">
+			<a href="https://twitter.com/districtsounduk">Twitter</a><br>
+			<a href="https://www.facebook.com/districtsounduk">Facebook</a><br>
+			<a href="https://soundcloud.com/districtsounduk">Soundcloud</a><br>
+		</div>
 	</div>
 	</body>
 	</html>
