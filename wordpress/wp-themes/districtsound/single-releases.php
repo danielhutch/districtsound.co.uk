17d2a487d714a6f98fff7628d94131d3922e2f59
danielhutch <hutch.daniel@gmail.com>
2014-03-30 13:34:15 +0100

    Single Release page styles

--- wordpress/wp-themes/districtsound/single-releases.php
+++ wordpress/wp-themes/districtsound/single-releases.php
@@ -1,21 +1,25 @@
 <?php get_header(); ?>
 
-<div id="release">
+<div id="article">
 
 	<!-- <p>This is the single-releases.php file</p> -->
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-		<img src="<?php the_field( 'cover_large' ); ?>">
-		<h3><?php the_title(); ?></h3>
-		<?php the_field( 'release_number' ); ?>
-		<?php the_field( 'artist' ); ?>
-		<?php the_field( 'title' ); ?>
-		<?php the_field( 'tracks' ); ?>
-		<?php the_field( 'buy_digital' ); ?>
-		<?php the_field( 'buy_vinyl' ); ?>
+		<div id="releasecover">
+			<div id="releasecoversub">
+				<img src="<?php the_field( 'cover_large' ); ?>">
+			</div>
+		</div>
+		<div id="releaseinfo">
+			<div id="releaseinfosub">
+				<h4><?php the_field( 'release_number' ); ?></h4>
+				<h3 class="releases_artist"><?php the_field( 'artist' ); ?></h3>
+				<h3 class="releases_title"><?php the_field( 'title' ); ?></h3>
+				<p><?php the_field( 'tracks' ); ?></p>
+				<p><a href="<?php the_field( 'buy_digital' ); ?>">Download Release</a><!-- | <a href="<?php the_field( 'buy_vinyl' ); ?>">Vinyl</a>--></p>
+			</div>
+		</div>
 		<?php the_field( 'soundcloud_api' ); ?>
-		
 
 	<?php endwhile; else: ?>
 
