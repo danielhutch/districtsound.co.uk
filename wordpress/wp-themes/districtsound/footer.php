121248e5fd17cccd5b3c7c23fa5b5e53539832c2
danielhutch <hutch.daniel@gmail.com>
2014-06-02 12:31:57 +0100

    Footer icons WIP
    Attempt to use icons as links in the footer

--- wordpress/wp-themes/districtsound/footer.php
+++ wordpress/wp-themes/districtsound/footer.php
@@ -7,10 +7,17 @@
 			<a href="mailto:info@districtsound.co.uk">info@districtsound.co.uk</a><br>
 			<a href="http://www.danielhutchinson.co.uk">Site by Daniel</a></p>
 		</div>
+		<!-- 
 		<div class="footerlinks">
 			<a href="https://twitter.com/districtsounduk">Twitter</a><br>
 			<a href="https://www.facebook.com/districtsounduk">Facebook</a><br>
 			<a href="https://soundcloud.com/districtsounduk">Soundcloud</a><br>
+		</div>
+		 -->
+		<div class="elsewhere">
+			<a href="https://soundcloud.com/districtsounduk"><img src="http://www.districtsound.co.uk/wp-content/themes/districtsound/images/soundcloud.png" alt="Soundcloud logo"/></a>
+			<a href="https://www.facebook.com/districtsounduk"><img src="http://www.districtsound.co.uk/wp-content/themes/districtsound/images/facebook.png" alt="Facebook logo" /></a>
+			<a href="https://twitter.com/districtsounduk"><img src="http://www.districtsound.co.uk/wp-content/themes/districtsound/images/twitter.png" alt="Twitter logo" /></a>
 		</div>
 	</div>
 
